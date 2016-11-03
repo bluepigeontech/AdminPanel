97e927e8ab56e53a3e99881f7b8c7623e415ee95
bhuwan-arora <bhuwanarora67@gmail.com>
2016-11-03 12:15:04 +0530

    production env

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -23,6 +23,8 @@ server 'ec2-35-160-177-190.us-west-2.compute.amazonaws.com', user: 'ubuntu', rol
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
 # you can see them in [net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start)
 # set it globally
+
+
   set :ssh_options, {
     user: 'ubuntu',
     keys: %w(/home/ubuntu/bluepigeon/bluepigeon-admin-aws-ec2.pem),
